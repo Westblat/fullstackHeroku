2a150a5188f22bf05a0abf1d7dc930306790bf32
Joonas Malinen <joonas.malinen@hotmail.fi>
2021-11-05 11:54:23 +0200

    Added build, hope to god it works

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 app.use(cors())
+app.use(express.static('build'))
 
 //----------------------------------------------------------------------------------------------
 
